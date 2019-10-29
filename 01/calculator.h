44c046db9104f5985c3ba42042cb53898b9ff2c7
NickGorev <gnamail62@gmail.com>
2019-10-29 14:23:57 +0300

    initial commit

---
+++ 01/calculator.h
@@ -0,0 +1,16 @@
+#ifndef CALCULATOR_H_
+#define CALCULATOR_H_
+
+class Calculator {
+public:
+	explicit Calculator(const char* input) : input(input) {}
+	int getNumber();
+	char getOperator();
+	int getMonomial();
+	int calculate();
+
+	const char* input;
+};
+
+
+#endif /* CALCULATOR_H_ */
---
+++ 01/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "calculator.h"
+
+int main(int argc, char *argv[]) {
+	if (argc == 1) {
+		std::cout << "error: no arithmetic expression" << std::endl;
+		return 1;
+	}
+
+	try {
+		int result = Calculator(argv[1]).calculate();
+		std::cout << result << std::endl;
+	}
+	catch (std::exception& e) {
+		std::cout << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
+
